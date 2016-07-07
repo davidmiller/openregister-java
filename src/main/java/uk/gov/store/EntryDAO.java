0cd74b2a0c4cc16ff5647302a20ccb9cf74c55d0
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-07-07 10:49:28 +0100

    make compatible with postgres 9.4
    postgres 9.4 doesn't support CREATE INDEX IF NOT EXISTS. rewrite to support this.

--- src/main/java/uk/gov/store/EntryDAO.java
+++ src/main/java/uk/gov/store/EntryDAO.java
@@ -5,11 +5,31 @@ import org.skife.jdbi.v2.sqlobject.stringtemplate.UseStringTemplate3StatementLoc
 import uk.gov.mint.Entry;
 
 @UseStringTemplate3StatementLocator("/sql/entry.sql")
-public interface EntryDAO {
-    @SqlUpdate
-    void ensureSchema();
+public abstract class EntryDAO {
+    public void ensureSchema() {
+        ensureEntryTable();
+        if (!entryIndexExists()) {
+            createEntryIndex();
+        }
+        ensureCurrentEntryNumberTable();
+        ensureCurrentEntryNumberInit();
+    }
+
+    @SqlQuery("SELECT to_regclass('public.entry_sha256hex_index');")
+    protected abstract boolean entryIndexExists();
+
+    @SqlUpdate("create index entry_sha256hex_index on entry (sha256hex);")
+    protected abstract void createEntryIndex();
+
+    @SqlUpdate("create table if not exists entry (" +
+            "entry_number integer primary key," +
+            "sha256hex varchar," +
+            "timestamp timestamp default (now() at time zone 'utc'))")
+    public abstract void ensureEntryTable();
+
     @SqlUpdate("create table if not exists current_entry_number(value integer not null)")
-    void ensureCurrentEntryNumberTable();
+    public abstract void ensureCurrentEntryNumberTable();
+
     @SqlUpdate(
             "   insert into current_entry_number(value)\n" +
             "        select (\n" +
@@ -21,14 +41,14 @@ public interface EntryDAO {
             "        where not exists (\n" +
             "            select 1 from current_entry_number\n" +
             "        );\n")
-    void ensureCurrentEntryNumberInit();
+    public abstract void ensureCurrentEntryNumberInit();
 
     @SqlBatch("insert into entry(entry_number, sha256hex) values(:entry_number, :sha256hex)")
-    void insertInBatch(@BindBean Iterable<Entry> entries);
+    public abstract void insertInBatch(@BindBean Iterable<Entry> entries);
 
     @SqlQuery("select value from current_entry_number")
-    int currentEntryNumber();
+    public abstract int currentEntryNumber();
 
     @SqlUpdate("update current_entry_number set value=:entry_number")
-    void setEntryNumber(@Bind("entry_number") int currentEntryNumber);
+    public abstract void setEntryNumber(@Bind("entry_number") int currentEntryNumber);
 }
--- src/main/resources/sql/entry.sql
+++ src/main/resources/sql/entry.sql
@@ -1,6 +1,2 @@
 group EntryDAO;
 
-ensureSchema() ::= <<
-   create table if not exists entry (entry_number integer primary key, sha256hex varchar, timestamp timestamp default (now() at time zone 'utc'));
-   create index if not exists entry_sha256hex_index on entry (sha256hex);
->>
