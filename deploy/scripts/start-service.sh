807a99829aee415c67684865a4911f0914bff8ff
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-11-28 16:08:21 +0000

    migrate database outside of the app itself
    This changes the app so that it doesn't migrate its own database on app
    startup.  This means that whenever the app runs, something else needs to
    ensure the database has been migrated appropriately.  There are four
    situations:
    
     - for actual deployment, the start-service.sh script runs the
       migration (using a command I manually tested on an existing EC2 box)
     - for ./run-application, we have an extra gradle task migrateMainDb
     - for functional tests, RegisterRule migrates the database before
       running the tests
     - for conformance tests, there was already the appropriate usage of
       {create,drop}_schema.sh, but the waitForApplicationToStart()
       operation broke because the app didn't return a 200 response on the
       application port when the database hadn't been migrated already.  I
       replaced this with a call to /healthcheck on the admin port instead.
    
    The longer-term goal is to entirely separate application deployment from
    database deployment, so that we can have automatic rollbacks for apps
    and have a separate deployment pipeline for database migrations.

--- deploy/scripts/start-service.sh
+++ deploy/scripts/start-service.sh
@@ -11,6 +11,13 @@ aws s3 cp s3://${CONFIG_BUCKET}/${REGISTER_NAME}/openregister/config.yaml /srv/o
 aws s3 cp s3://${CONFIG_BUCKET}/registers.yaml /srv/openregister-java --region eu-west-1
 aws s3 cp s3://${CONFIG_BUCKET}/fields.yaml /srv/openregister-java --region eu-west-1
 
+docker run --name=openregister-migrate \
+    --volume /srv/openregister-java:/srv/openregister-java \
+    jstepien/openjdk8 java -Xmx8g -Dfile.encoding=UTF-8 \
+    -jar /srv/openregister-java/openregister-java.jar \
+    db migrate /srv/openregister-java/config.yaml
 docker run -d --name=openregister -p 80:8080 \
     --volume /srv/openregister-java:/srv/openregister-java \
-    jstepien/openjdk8 java -Xmx8g -Dfile.encoding=UTF-8 -DregistersYaml=/srv/openregister-java/registers.yaml -DfieldsYaml=/srv/openregister-java/fields.yaml -jar /srv/openregister-java/openregister-java.jar server /srv/openregister-java/config.yaml
+    jstepien/openjdk8 java -Xmx8g -Dfile.encoding=UTF-8 -DregistersYaml=/srv/openregister-java/registers.yaml -DfieldsYaml=/srv/openregister-java/fields.yaml \
+    -jar /srv/openregister-java/openregister-java.jar \
+    server /srv/openregister-java/config.yaml
